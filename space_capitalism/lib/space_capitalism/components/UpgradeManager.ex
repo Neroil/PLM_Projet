20dea6277ebb1808d4b86bd6ec6fec9d30430619
GuillaumeDnt2 <guillaume.dunant@heig-vd.ch>
2025-06-09 19:01:39 +0200

    Remove use Genserver because it was useless

--- space_capitalism/lib/space_capitalism/components/UpgradeManager.ex
+++ space_capitalism/lib/space_capitalism/components/UpgradeManager.ex
@@ -1,14 +1,4 @@
 defmodule UpgradeManager do
-  use GenServer
-
-  def start_link(_) do
-    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
-  end
-
-  @impl true
-  def init(state) do
-    {:ok, state}
-  end
 
   def get_upgrades() do
     %{
